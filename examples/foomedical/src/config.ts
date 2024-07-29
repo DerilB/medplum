9d4168462d636158cef71717a6182b5a02db71d0
DerilB <128557270+DerilB@users.noreply.github.com>
2024-07-29 12:40:00 -0500

    Update config.ts

--- examples/foomedical/src/config.ts
+++ examples/foomedical/src/config.ts
@@ -1,5 +1,8 @@
 // Default values work on localhost:3000 and foomedical.com
 // Replace these values with your own values for production
-export const MEDPLUM_PROJECT_ID = '2a4b77f2-4d4e-43c6-9b01-330eb5ca772f';
+export const MEDPLUM_PROJECT_ID = '';
 export const MEDPLUM_GOOGLE_CLIENT_ID = '220709119901-j7kdbb1hoq15h0d5ssk53n99bmofun6k.apps.googleusercontent.com';
-export const MEDPLUM_RECAPTCHA_SITE_KEY = '6LfHdsYdAAAAAC0uLnnRrDrhcXnziiUwKd8VtLNq';
+export const MEDPLUM_GOOGLE_CLIENT_SECRET= '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';
+export const MEDPLUM_RECAPTCHA_SITE_KEY = '6Lex6BcqAAAAAD8zk9NDc93QIlkslpQup3kcGB3T';
+export const MEDPLUM_RECAPTCHA_SECRET_KEY = '6Lex6BcqAAAAALPslHgqGZY_Rsnh_PL3oiBlg3mO';  
+ 
